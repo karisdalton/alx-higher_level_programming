7f6339648d41bb32facf2159702b785ab3e3e829
karisdalton <daltonkaris2000@gmail.com>
2022-12-01 06:26:48 -0800

    module imports in python

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def add(a, b):
+    """Addition fuction
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        Return value of a+b
+    """
+    return (a + b)
